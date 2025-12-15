d050f196ddbad3709fd48e0d91a853c763dc9600
BwdYeti <BwdYeti@gmail.com>
2025-12-15 11:06:08 -0500

    Initial commit of FE4XNA

---
+++ FE4XNA/FE4XNA/Activity1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Views;
+
+namespace Android_MonoGame_Test
+{
+    [Activity(Label = "FE4XNA"
+        , MainLauncher = true
+        , Icon = "@drawable/icon"
+        , Theme = "@style/Theme.Splash"
+        , AlwaysRetainTaskState = true
+        , LaunchMode = Android.Content.PM.LaunchMode.SingleInstance
+        , ScreenOrientation = ScreenOrientation.Landscape //ScreenOrientation.SensorLandscape
+        , ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize)]
+    public class Activity1 : Microsoft.Xna.Framework.AndroidGameActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException +=
+                new UnhandledExceptionEventHandler(FE4XNA.ExceptionLogger.Handler);
+
+            var g = new FE4XNA.Game1(new string[0]);
+            SetContentView((View)g.Services.GetService(typeof(View)));
+            g.Run();
+        }
+    }
+}
+
